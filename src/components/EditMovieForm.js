ac348c8b14e8fae2bf02e5bec71b67db702fd8fa
Markeisha <markeisha94@gmail.com>
2022-01-26 13:21:49 -0600

    updated editMovieForm

--- src/components/EditMovieForm.js
+++ src/components/EditMovieForm.js
@@ -23,6 +23,9 @@ const EditMovieForm = (props) => {
             .then(res=>{
                 setMovie(res.data);
             })
+			.catch(err => {
+				console.log(err)
+			})
 	}, [id]);
 	
 	const handleChange = (e) => {
